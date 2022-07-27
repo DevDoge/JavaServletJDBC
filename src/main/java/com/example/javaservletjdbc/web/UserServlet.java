d7bcb4e5097534fdf77fdca1ca70cb24f35d90e5
DevDoge <lhuthefranc@hotmail.com>
2022-07-27 16:33:49 -0300

    Servlet updated

--- src/main/java/com/example/javaservletjdbc/web/UserServlet.java
+++ src/main/java/com/example/javaservletjdbc/web/UserServlet.java
@@ -1,23 +1,118 @@
 package com.example.javaservletjdbc.web;
 
+import com.example.javaservletjdbc.dao.UserDAO;
+import com.example.javaservletjdbc.model.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/")
 public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UserDAO userDAO;
+
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    private void listUser(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        List<User> listUser = userDAO.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        User existingUser = userDAO.selectUserById(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+        request.setAttribute("user", existingUser);
+        dispatcher.forward(request, response);
+    }
+
+    private void insertForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        User newUser = new User(nome, email, country);
+        userDAO.insertUser(newUser);
+        response.sendRedirect("list");
+    }
+
+    private void updateUser(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+
+        User updatedUser = new User(id, nome, email, country);
+        userDAO.updateUser(updatedUser);
+        response.sendRedirect("liist");
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDAO.deleteUser(id);
+        response.sendRedirect("list");
+    }
+
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         doGet(request, response);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         String action = request.getServletPath();
 
+        try {
+            switch (action) {
+                case "/new":
+                    showNewForm(request, response);
+                    break;
 
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
+
+                case "/insert":
+                    insertForm(request, response);
+                    break;
+
+                case "/update":
+                    updateUser(request, response);
+                    break;
+
+                case "/delete":
+                    deleteUser(request, response);
+                    break;
+
+                default:
+                    listUser(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
     }
-
 }
